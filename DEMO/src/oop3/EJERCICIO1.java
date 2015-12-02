98ccc1cd8b43e70439e05ad8fbb5c2dd27fb35e9
Isabel <Isabel@Pardo>
2015-12-01 20:14:05 -0500

    Círculo-Visibilidad-Tv

---
+++ DEMO/src/oop3/EJERCICIO1.java
@@ -0,0 +1,14 @@
+package oop3;
+
+public class EJERCICIO1 {
+
+	
+		
+		public static int control=12;
+		public static void mostrar(){
+			System.out.println("Calcular Mostrar");
+	}
+		
+	
+
+}
---
+++ DEMO/src/oop3/VISIBILIDAD.java
@@ -0,0 +1,18 @@
+package oop3;
+
+public class VISIBILIDAD {
+
+	public void MetodoPublico() {
+		System.out.println("Metodo Publico");
+		MetodoPrivado();
+	}
+
+	private void MetodoPrivado() {
+		System.out.println("Metodo Privado");
+	
+	}
+void MetodoPaquete(){
+	System.out.println("Metodo Paquete");
+	
+}
+}
